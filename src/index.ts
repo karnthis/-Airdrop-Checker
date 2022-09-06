d0606ad59f70a8de5d0f1adfe3e0244fd64b2f80
Erin Rivas <erin@atomicbear.com>
2022-09-06 12:36:48 -0500

    ts refactor

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import mysql from 'mysql'
+
+const app = express()
+
+const {
+    PORT,
+    USER,
+    PASSWORD,
+    SERVER,
+    DATABASE
+} = process.env
+const usedPort = Number(PORT) || 3000
+
+const mysqlConfig = {
+    user: USER,
+    password: PASSWORD,
+    server: SERVER,
+    database: DATABASE
+};
+
+app.get('/:wallet', function (req, res) {
+    mysql.connect(mysqlConfig, function (err) {
+        if (err) {
+            console.log(err)
+            res.error(err)
+        } else {
+            const mysqlRequest = new mysql.Request()
+
+            mysqlRequest.query(`select * from example where wallet = '${req.params.wallet}'`,
+              function (err, records) {
+                  if (err) {
+                      console.log(err)
+                      res.error(err)
+                  } else {
+                      res.send(records)
+                  }
+              })
+        }
+    })
+})
+
+app.listen(usedPort, function () {
+    console.log(`Server is listening at port ${ usedPort }`)
+})
